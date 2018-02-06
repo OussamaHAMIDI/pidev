6c376c4da1f0b47ae5e413cb6cef30a27932dd5f
HamdiMegdiche <Hamdi-Megdiche@outlook.fr>
2018-02-06 16:18:04 +0100

    fix connexion

--- Desktop/src/DataStorage/MyDB.java
+++ Desktop/src/DataStorage/MyDB.java
@@ -10,7 +10,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import pkg3a12_workshop01.Main;
+import main.Main;
 
 /**
  *
