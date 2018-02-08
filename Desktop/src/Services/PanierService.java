e152fc96b9ddeb8d80a13e69cfce4dc31e4cc178
Montazio <30464013+Montazio@users.noreply.github.com>
2018-02-09 00:16:16 +0100

    FinalPanierService

--- Desktop/src/Services/PanierService.java
+++ Desktop/src/Services/PanierService.java
@@ -141,7 +141,9 @@ return 1;
             ResultSet rs= this.connexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
                     .executeQuery("SELECT * FROM produitpanier WHERE id_panier = '" + panierId + "'");
             while (rs.next()) {
-                produits.add(new ProduitPanier(rs.getInt("idproduit"),
+                produits.add(new ProduitPanier(rs.getFloat("quantitevendu"),
+                        rs.getFloat("poidsvendu"),
+                        rs.getFloat("prixvente"),rs.getInt("idproduit"),
                         rs.getString("reference"),
                         rs.getString("libelle"),
                         rs.getString("description"),
@@ -149,10 +151,8 @@ return 1;
                         rs.getString("taille"),
                         rs.getString("couleur"),
                         rs.getString("texture"),
-                        rs.getFloat("poids"),
-                        rs.getFloat("quantitevendu"),
-                        rs.getFloat("poidsvendu"),
-                        rs.getFloat("prixvente")));
+                        rs.getFloat("poids")
+                        ));
             }
         } catch (SQLException e) {
             System.out.println("erreur" + e.getMessage());
@@ -162,27 +162,127 @@ return 1;
 
     @Override
     public int ajouterProduitPanier(ProduitPanier produit, int idPanier) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
+            String req = "INSERT INTO produitpanier (idpanier,idproduit,reference,libelle,description,prix,taille,couleur,texture,poids,idBoutique,quantiteVendu,poidsVendu,prixVendu) values (?,?,?,?,?,?,?,?,?,?,?,?)";                                              
+              try {
+            ps = connexion.prepareStatement(req);
+            
+            ps.setInt(1, idPanier);
+            ps.setInt(2, produit.getIdProduit());
+            ps.setString(3, produit.getReference());
+            ps.setString(4, produit.getLibelle());
+            ps.setString(5, produit.getDescription());
+            ps.setFloat(6, produit.getPrix());
+            ps.setString(7, produit.getTaille());
+            ps.setString(8, produit.getCouleur());
+            ps.setString(9, produit.getTexture());
+            ps.setFloat(10, produit.getPoids());
+            ps.setInt(11, produit.getIdBoutique());
+            ps.setFloat(12, produit.getQuantiteVendue());
+            ps.setFloat(13, produit.getPoidsVendu());
+            ps.setFloat(14, produit.getPrixVente());
+            ps.executeUpdate(req);
+            return 1;
+        } catch (SQLException ex) {
+            Logger.getLogger(PanierService.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Echec d'ajout");
+            return 0;
+        }
     }
 
     @Override
     public int modifierProduitPanier(ProduitPanier produit, int idPanier) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         String req = "UPDATE produitpanier SET reference=?,libelle=?,description=?,prix=?,taille=?,couleur=?,texture=?,poids=?,idBoutique=?,quantiteVendu=?,poidsVendu=?,prixVendu=? where idpanier=? and idproduit=?";                                              
+              try {
+            ps = connexion.prepareStatement(req);
+            
+            ps.setInt(13, idPanier);
+            ps.setInt(14, produit.getIdProduit());
+            ps.setString(1, produit.getReference());
+            ps.setString(2, produit.getLibelle());
+            ps.setString(3, produit.getDescription());
+            ps.setFloat(4, produit.getPrix());
+            ps.setString(5, produit.getTaille());
+            ps.setString(6, produit.getCouleur());
+            ps.setString(7, produit.getTexture());
+            ps.setFloat(8, produit.getPoids());
+            ps.setInt(9, produit.getIdBoutique());
+            ps.setFloat(10, produit.getQuantiteVendue());
+            ps.setFloat(11, produit.getPoidsVendu());
+            ps.setFloat(12, produit.getPrixVente());
+            ps.executeUpdate(req);
+            return 1;
+        } catch (SQLException ex) {
+            Logger.getLogger(PanierService.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Echec de mise a jour ligne panier");
+            return 0;
+        }
     }
 
     @Override
     public int supprimerProduitPanier(int produitId, int idPanier) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+          String req = "Delete from produitpanier where idproduit=?,idpanier=? ";
+        try {
+            ps = connexion.prepareStatement(req);
+            ps.setInt(1, produitId);
+            ps.setInt(2, idPanier);
+            ps.executeUpdate(req);
+            return 1;
+        } catch (SQLException ex) {
+            Logger.getLogger(PanierService.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Echec de suppression ligne produit");
+            return 0;
+        }
     }
 
   @Override
     public List<Panier> rechercherPaniersUtilisateur(int userId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Panier> paniers = null;
+        try {
+            ResultSet rs= this.connexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
+                    .executeQuery("SELECT * FROM panier WHERE userid = '" + userId + "'");
+            while (rs.next()) {
+                paniers.add(new Panier(rs.getInt("id"),
+                        rs.getInt("userid"),
+                        rs.getObject("datecreation", LocalDateTime.class),
+                        rs.getObject("datelivraison", LocalDateTime.class),
+                        rs.getDouble("totalttc"),
+                        rs.getDouble("fraislivraison"),
+                        rs.getString("status"),
+                        rs.getString("modepaiement"),
+                        rs.getBoolean("estlivre"),
+                        rs.getBoolean("estpaye"),
+                        rechercherProduitsPanier(rs.getInt("id"))));
+            }
+        } catch (SQLException e) {
+            System.out.println("erreur" + e.getMessage());
+        }
+        return paniers;
     }
 
     @Override
     public List<Panier> rechercherPaniersUtilisateur(int userId, String status) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Panier> paniers = null;
+        try {
+            ResultSet rs= this.connexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
+                    .executeQuery("SELECT * FROM panier WHERE userid = '" + userId + "' AND status='"+status+"'");
+            while (rs.next()) {
+                paniers.add(new Panier(rs.getInt("id"),
+                        rs.getInt("userid"),
+                        rs.getObject("datecreation", LocalDateTime.class),
+                        rs.getObject("datelivraison", LocalDateTime.class),
+                        rs.getDouble("totalttc"),
+                        rs.getDouble("fraislivraison"),
+                        rs.getString("status"),
+                        rs.getString("modepaiement"),
+                        rs.getBoolean("estlivre"),
+                        rs.getBoolean("estpaye"),
+                        rechercherProduitsPanier(rs.getInt("id"))));
+            }
+        } catch (SQLException e) {
+            System.out.println("erreur" + e.getMessage());
+        }
+        return paniers;
     }
     
 }
