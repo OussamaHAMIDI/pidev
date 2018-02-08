fec4e24609a2aad3e421e42f8eeb686ecc4e22b9
HamdiMegdiche <Hamdi-Megdiche@outlook.fr>
2018-02-08 15:55:34 +0100

    fix user

--- Desktop/src/IServices/IPanier.java
+++ Desktop/src/IServices/IPanier.java
@@ -1,21 +1,14 @@
+package IServices;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package IServices;
 
-<<<<<<< HEAD
+
+import Entities.Panier;
 import Entities.Produit;
-import Entities.*;
-=======
-
-import Entities.Produit;
-import entities.*;
-
-import Entities.*;
-
->>>>>>> 6deb11795fc702b49fc733d6fb1a218c6b0df91c
 import java.util.List;
 
 /**
@@ -23,6 +16,7 @@ import java.util.List;
  * @author monta
  */
 public interface IPanier {
+    
     public int ajouterProduitPanier(Produit prod,Panier pan);
     public int supprimerProduitPanier(Produit prod,Panier pan);
     public List<Panier> rechercherPaniersUtilisateur(String userId);
