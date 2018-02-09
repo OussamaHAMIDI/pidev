da9f78eee7357bc0b739bdd98cbd8eb02663b5b8
ImenBenA <imen.benabderrahmen@esprit.tn>
2018-02-09 11:44:10 +0100

    Merge origin/master

---
+++ Desktop/src/main/MainProduit.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import Entities.Produit;
+import Services.ProduitService;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author benab
+ */
+public class MainProduit {
+    
+    static Connection connexion;
+    static Statement state;
+    final static String url = "jdbc:mysql://localhost:3306/souk";
+    final static String user = "root";
+    final static String password = "";
+    
+    public static void main(String[] args) {
+    Produit p = new Produit();
+    
+    
+    ProduitService ps = new ProduitService();
+    ps.ajouterProduit(p);
+    
+   
+}
+}
