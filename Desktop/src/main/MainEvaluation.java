4cd8f60bcc46330b075385c0111ad42adf2862d2
ImenBenA <imen.benabderrahmen@esprit.tn>
2018-02-11 12:19:20 +0100

    Merge origin/master
    Conflicts:
    	.gitignore
    	Desktop/nbproject/private/private.properties
    	Desktop/nbproject/private/private.xml
    	Desktop/nbproject/project.properties
    	Desktop/src/DataStorage/MyDB.java
    	Desktop/src/Entities/Boutique.java
    	Desktop/src/Entities/Evaluation.java
    	Desktop/src/Entities/Panier.java
    	Desktop/src/Entities/Produit.java
    	Desktop/src/Entities/ProduitPanier.java
    	Desktop/src/Entities/Reclamation.java
    	Desktop/src/Entities/Stock.java
    	Desktop/src/Entities/User.java
    	Desktop/src/IServices/IBoutique.java
    	Desktop/src/IServices/IEvaluation.java
    	Desktop/src/IServices/IPanier.java
    	Desktop/src/IServices/IProduit.java
    	Desktop/src/IServices/IReclamation.java
    	Desktop/src/IServices/IStock.java
    	Desktop/src/IServices/IUser.java
    	Desktop/src/Services/BoutiqueService.java
    	Desktop/src/Services/EvaluationService.java
    	Desktop/src/Services/PanierService.java
    	Desktop/src/Services/ProduitService.java
    	Desktop/src/Services/ReclamationService.java
    	Desktop/src/Services/StockService.java
    	Desktop/src/Services/UserService.java

--- Desktop/src/main/MainEvaluation.java
+++ Desktop/src/main/MainEvaluation.java
@@ -5,36 +5,61 @@
  */
 package main;
 
+import Entities.Boutique;
 import Entities.Evaluation;
+import Entities.Produit;
+import Entities.User;
+import Services.BoutiqueService;
 import Services.EvaluationService;
+import Services.UserService;
 import Utils.Enumerations.TypeReclamation;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 
 /**
  *
  * @author benab
  */
 public class MainEvaluation {
-    
+
     public static void main(String[] args) {
-        
-   
+
         LocalDateTime now;
         //now = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-        TypeReclamation type = TypeReclamation.Boutique;
+        TypeReclamation type1 = TypeReclamation.Boutique;
+        TypeReclamation type2 = TypeReclamation.Produit;
+
+        Produit p1 = new Produit();
+        Produit p2 = new Produit();
+        List<Produit> produits = new ArrayList<Produit>();
+
+        produits.add(p1);
+        produits.add(p2);
+        p1.setIdProduit(1);
+        Boutique boutique = new Boutique(5,"hello", produits, LocalDateTime.MIN, "36rue");
+        BoutiqueService bs = new BoutiqueService();
+        boutique.setUserId(1);
+        bs.ajouterBoutique(boutique);
+        User user = new User();
+        user.setId(1);
         //Evaluation e1 = new Evaluation(1,1,1,now,type,8);
-        Evaluation e2 = new Evaluation(2,2,2,null,type,5);
-        
+        Evaluation e2 = new Evaluation(3, user, boutique, null, type1, 5);
+        Evaluation e3 = new Evaluation(5,user,p1,null,type2,5);
         EvaluationService es = new EvaluationService();
-        
+
         boolean a = es.ajouterEvaluation(e2);
+        boolean b = es.ajouterEvaluation(e3);
+        int idEvaluation = e3.getId();
+        boolean x =es.supprimerEvaluation(idEvaluation);
+        
+        
 //        es.ajouterEvaluation(e1);
 //        es.supprimerEvaluation(e2);
 //        es.supprimerEvaluation(e2.getId());
-        
-        
+
     }
-    
+
 }
--- Desktop/src/main/MainReclamation.java
+++ Desktop/src/main/MainReclamation.java
@@ -5,10 +5,17 @@
  */
 package main;
 
+import Entities.Boutique;
+import Entities.Produit;
 import Entities.Reclamation;
+import Entities.User;
+import Services.BoutiqueService;
 import Services.ReclamationService;
 import Utils.Enumerations;
+import Utils.Enumerations.TypeReclamation;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -19,18 +26,32 @@ public class MainReclamation {
      public static void main(String[] args) {
         
    
-        LocalDateTime now;
-        //now = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-        Enumerations.TypeReclamation type = Enumerations.TypeReclamation.Boutique;
+        TypeReclamation type1 = TypeReclamation.Boutique;
+        TypeReclamation type2 = TypeReclamation.Produit;
+
+        Produit p1 = new Produit();
+        Produit p2 = new Produit();
+        List<Produit> produits = new ArrayList<Produit>();
+
+        produits.add(p1);
+        produits.add(p2);
+        p1.setIdProduit(1);
+        Boutique boutique = new Boutique(5,"hello", produits, LocalDateTime.MIN, "36rue");
+        BoutiqueService bs = new BoutiqueService();
+        boutique.setUserId(1);
+        User user = new User();
+        user.setId(1);
         //Evaluation e1 = new Evaluation(1,1,1,now,type,8);
-        Reclamation r2 = new Reclamation(2,2,2,"description",null,type);
-        
+        Reclamation r1 = new Reclamation(user, boutique,"maaasset", null, type1);
+        Reclamation r2 = new Reclamation(user,p1,"hloww barcha",null,type2);
         ReclamationService rs = new ReclamationService();
+
+        boolean a = rs.ajouterReclamation(r1);
+        boolean b = rs.ajouterReclamation(r2);
+        List<Reclamation> reclamations = new ArrayList<Reclamation>();
+        reclamations = rs.rechercherReclamationBoutique(boutique);
         
-            boolean a = rs.ajouterReclamation(r2);
-//          es.ajouterEvaluation(e1);
-            a = rs.supprimerReclamation(r2);
-//        es.supprimerEvaluation(e2.getId());
+        //boolean x = es.supprimerReclamation(r1);
     
 }
 }
