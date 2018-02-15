b73d87e97b4d20f85a21d95def31f5a9b04aba6d
ImenBenA <imen.benabderrahmen@esprit.tn>
2018-02-15 12:17:25 +0100

    Merge origin/master
    Conflicts:
    	Desktop/nbproject/project.properties

---
+++ Desktop/src/Presentation/HistoriqueClientController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+/**
+ * FXML Controller class
+ *
+ * @author benab
+ */
+public class HistoriqueClientController implements Initializable {
+    @FXML
+    private TableView<?> historiqueClient;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
