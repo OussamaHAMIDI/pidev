a3a9c11039d5768861ebeb6efe81cb4237bacfd8
ImenBenA <imen.benabderrahmen@esprit.tn>
2018-02-27 23:03:31 +0100

    Merge origin/master
    Conflicts:
    	Desktop/nbproject/private/private.xml

--- Desktop/src/Services/ProduitService.java
+++ Desktop/src/Services/ProduitService.java
@@ -103,12 +103,9 @@ public class ProduitService implements IProduit {
             ResultSet result = connexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
                     .executeQuery("SELECT * FROM produit WHERE id = " + id);
             if (result.first()) {
-<<<<<<< HEAD
-                return produit = new Produit(result.getInt("id"), result.getString("reference"), result.getString("libelle"), result.getString("description"),result.getFloat("prix"), result.getString("taille"), result.getString("couleur"), result.getString("texture"), result.getFloat("poids"), bs.chercherBoutiqueParID(result.getInt("id_boutique")),Utils.Utils.getLocalDateTime(result.getString("date_creation")),result.getBinaryStream("photo"));
-=======
+
                 produit = new Produit(result.getInt("id"), result.getString("reference"), result.getString("libelle"), result.getString("description"),result.getFloat("prix"), result.getString("taille"), result.getString("couleur"), result.getString("texture"), result.getFloat("poids"), /*bs.chercherBoutiqueParID(result.getInt("id_boutique"))*/ null,Utils.Utils.getLocalDateTime(result.getString("date_creation")),result.getBinaryStream("photo"));
                 return produit;
->>>>>>> ad2edede6be6af227f41dd3d171fc4466098c250
             }
         } catch (SQLException ex) {
              Logger.getLogger(ProduitService.class.getName()).log(Level.SEVERE, null, ex);
