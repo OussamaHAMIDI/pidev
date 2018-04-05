003c9a151ab4e80732a75659d2d7b0ce4644c2e1
azzadaghmouri <36170462+azzadaghmouri@users.noreply.github.com>
2018-04-05 20:26:15 +0100

    hhh

--- WEB/app/AppKernel.php
+++ WEB/app/AppKernel.php
@@ -18,7 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new SoukBundle\SoukBundle(),
             new UserBundle\UserBundle(),
-            new \FOS\UserBundle\FOSUserBundle()
+            new \FOS\UserBundle\FOSUserBundle(),
+            new BoutiqueBundle\BoutiqueBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
