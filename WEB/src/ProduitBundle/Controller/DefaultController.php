3f4b39fd487cd628da991f948f54f73dd70ff8ce
OussamaHAMIDI <oussama.hamidi@esprit.tn>
2018-04-06 15:43:07 +0100

    :outbox_tray:

---
+++ WEB/src/ProduitBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ProduitBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ProduitBundle:Default:index.html.twig');
+    }
+}
