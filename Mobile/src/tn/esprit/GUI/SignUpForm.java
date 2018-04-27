9218ff03864e39abcda23f36f573261e01f29871
HamdiMegdiche <Hamdi-Megdiche@outlook.fr>
2018-04-27 20:24:23 +0100

    :panda_face: amboulet mezyenin :panda_face:

--- Mobile/src/tn/esprit/GUI/SignUpForm.java
+++ Mobile/src/tn/esprit/GUI/SignUpForm.java
@@ -49,13 +49,14 @@ public class SignUpForm extends Form {
         center.setUIID("SignUpCenter");
         
         Container row1 = new Container(new GridLayout(1,2));
+        
         TextField firstName = new TextField();
         firstName.setUIID("SignUpField");
-        firstName.setHint("First Name");
+        firstName.setHint("Prénom");
         firstName.getHintLabel().setUIID("SignupFieldHint");
         TextField lastName = new TextField();
         lastName.setUIID("SignUpField");
-        lastName.setHint("Last Name");
+        lastName.setHint("Nom");
         lastName.getHintLabel().setUIID("SignupFieldHint");
         row1.addComponent(firstName);
         row1.addComponent(lastName);
@@ -65,24 +66,24 @@ public class SignUpForm extends Form {
         TextField email = new TextField();
         email.setUIID("SignUpField");
         center.addComponent(email);
-        email.setHint("Email Address");
+        email.setHint("Email");
         email.getHintLabel().setUIID("SignupFieldHint");
         
         TextField password = new TextField();
         password.setUIID("SignUpField");
         password.setConstraint(TextField.PASSWORD);
-        password.setHint("Choose Password");
+        password.setHint("Mot de passe");
         password.getHintLabel().setUIID("SignupFieldHint");
         center.addComponent(password);
         
         Container row4 = new Container(new BorderLayout());
-        Label code = new Label("+1");
+        Label code = new Label("+216");
         code.setUIID("SignUpLabel");
         row4.addComponent(BorderLayout.WEST, code);
         
         TextField phoneNumber = new TextField();
         phoneNumber.setUIID("SignUpField");
-        phoneNumber.setHint("Phone Number");
+        phoneNumber.setHint("Télephone");
         phoneNumber.getHintLabel().setUIID("SignupFieldHint");
         row4.addComponent(BorderLayout.CENTER, phoneNumber);
         
@@ -90,13 +91,13 @@ public class SignUpForm extends Form {
         
         this.addComponent(BorderLayout.CENTER, center);
         
-        Button getStarted = new Button("Get Started", res.getImage("right_arrow.png"));
+        Button getStarted = new Button("Creer compte", res.getImage("right_arrow.png"));
         getStarted.setGap(getStarted.getStyle().getFont().getHeight());
         getStarted.setUIID("SignUpButton");
         getStarted.setTextPosition(Component.LEFT);
         
         this.addComponent(BorderLayout.SOUTH, getStarted);
-        this.addCommand(new Command("Done") {
+        this.addCommand(new Command("Retour") {
 
             @Override
             public void actionPerformed(ActionEvent evt) {
--- Mobile/src/tn/esprit/GUI/TombolaForm.java
+++ Mobile/src/tn/esprit/GUI/TombolaForm.java
@@ -2,7 +2,9 @@ package tn.esprit.GUI;
 
 import tn.esprit.entities.Tombola;
 import com.codename1.components.MultiButton;
+import com.codename1.ui.Calendar;
 import com.codename1.ui.Command;
+import com.codename1.ui.Component;
 import com.codename1.ui.Container;
 import com.codename1.ui.Display;
 import com.codename1.ui.EncodedImage;
@@ -15,6 +17,7 @@ import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.layouts.GridLayout;
 import com.codename1.ui.plaf.Style;
 import com.codename1.ui.spinner.DateTimeSpinner;
 import com.codename1.ui.spinner.Picker;
@@ -28,7 +31,7 @@ import tn.esprit.Services.TombolaService;
 
 /**
  *
- * @author Imen BenAbderrahmen
+ * @author Hamdi Megdiche
  */
 public class TombolaForm extends Form {
 
@@ -48,31 +51,59 @@ public class TombolaForm extends Form {
         if (tombos != null) {
             for (Tombola t : tombos) {
 
-                EncodedImage placeholder = EncodedImage.createFromImage(Image.createImage(this.getWidth() / 4, this.getHeight() / 4, 0xFFFFFFFF), true);
-                
-                
+                EncodedImage placeholder = EncodedImage.createFromImage(Image.createImage(this.getWidth() / 3, this.getHeight() / 5, 0xFFFFFFFF), true);
                 Image img = URLImage.createToStorage(placeholder, t.getPhoto(), "http://localhost/pidev/WEB/web/uploads/" + t.getPhoto(),
                         URLImage.RESIZE_SCALE_TO_FILL);
+                
+                Container image = new Container();
+                image.setUIID("PhotoItem");
+                image.add(img);
 
                 MultiButton mb = new MultiButton(t.getTitre());
                 mb.setUIID("ListItem");
 //                mb.setNameLine1(t.getTitre());
-//                mb.setTextLine1(t.getDescription());
-                mb.setTextLine2("Date Ajout : \n" + t.getDateAjout());
-                mb.setTextLine3("Date Modification \n: " + t.getDateModif());
-                mb.setTextLine4("Date Tirage : \n" + t.getDateTirage());
-                mb.setIcon(img);
-                DateTimeSpinner dts = new DateTimeSpinner();
-                dts.setCurrentDate(new Date());
-                //dts.setEndDate(new Date(2019,1,1));
-                
+////                mb.setTextLine1(t.getDescription());
+//                mb.setTextLine2("Date Ajout : \n" + t.getDateAjout());
+//                mb.setTextLine3("Date Modification \n: " + t.getDateModif());
+//                mb.setTextLine4("Date Tirage : \n" + t.getDateTirage());
 
-//                tombolas.add(mb);
+                Label title = new Label(t.getTitre());
+                title.setUIID("TitleItem");
+                Container north = new Container(new FlowLayout(Component.CENTER));
+                north.addComponent(title);
+
+                mb.addComponent(BorderLayout.NORTH, north);
+
+                Container center = new Container(new BorderLayout());
+                center.add(BorderLayout.WEST, image);
+                
+                Container row = new Container(new GridLayout(7, 1));
+                row.add(new Label("Date Ajout :"));
+                Label l1 = new Label(t.getDateAjout());
+                l1.setUIID("RestItem");
+                row.add(l1);
+                row.add(new Label("Date Tirage :"));
+                Label l2 = new Label(t.getDateTirage());
+                l2.setUIID("RestItem");
+                row.add(l2);
+                row.add(new Label("Date Modification :"));
+                Label l3 = new Label(t.getDateModif());
+                l3.setUIID("RestItem");
+                row.add(l3);
+                row.add(new Label("Etat :"));
+                Label l4 = new Label(t.getEtat());
+                l4.setUIID("RestItem");
+                row.add(l4);
+                center.add(BorderLayout.CENTER, row);
+                
+                 mb.addComponent(BorderLayout.CENTER, center);
+
                 Picker dateTimePicker = new Picker();
                 dateTimePicker.setType(Display.PICKER_TYPE_DATE);
                 dateTimePicker.setDate(new Date());
-               
-                tombolas.add(FlowLayout.encloseCenter(mb, new Label(t.getEtat()))).add(dateTimePicker);
+
+                //tombolas.add(dateTimePicker);
+                tombolas.add(mb);//, new Label(t.getEtat())));//.add(dateTimePicker);
 
             }
         }
